2f4ce2847258039be8b9422f8a0a35c0f678b24f
zuoyangding <zuoyangding@yahoo.com>
2019-06-07 12:39:37 -0700

    mongo client with promise

--- mongo-node/index.js
+++ mongo-node/index.js
@@ -4,55 +4,85 @@ const operation = require('./operation');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-mongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
-    console.log("connected to server");
+// mongoClient.connect(url, (err, client) => {
+//     assert.equal(err, null);
+//     console.log("connected to server");
 
-    const db = client.db(dbname);
-    // const collection = db.collection("courses");
-    // collection.insertOne({
-    //     name: "CS180",
-    //     description: "Intro to JAVA programming"
-    // }, (err, result) => {
-    //     assert.equal(err, null);
-    //     console.log("After insert data:\n");
-    //     console.log(result.ops);
+//     const db = client.db(dbname);
+//     // const collection = db.collection("courses");
+//     // collection.insertOne({
+//     //     name: "CS180",
+//     //     description: "Intro to JAVA programming"
+//     // }, (err, result) => {
+//     //     assert.equal(err, null);
+//     //     console.log("After insert data:\n");
+//     //     console.log(result.ops);
 
-    //     collection.find({}).toArray((err, docs) => {
-    //         assert.equal(err, null);
-    //         console.log("Found:\n");
-    //         console.log(docs);
+//     //     collection.find({}).toArray((err, docs) => {
+//     //         assert.equal(err, null);
+//     //         console.log("Found:\n");
+//     //         console.log(docs);
 
-    //         db.dropCollection("courses", (err, result) => {
-    //             assert.equal(err, null);
-    //             client.close();
-    //         });
-    //     });
-    // });
+//     //         db.dropCollection("courses", (err, result) => {
+//     //             assert.equal(err, null);
+//     //             client.close();
+//     //         });
+//     //     });
+//     // });
 
-    operation.insertDocument(db, {name: "CS180", description: "Intro to java"}, "courses", (result) => {
-        console.log("Inserted Document:\n", result.ops);
+//     operation.insertDocument(db, {name: "CS180", description: "Intro to java"}, "courses", (result) => {
+//         console.log("Inserted Document:\n", result.ops);
 
-        operation.findDocument(db, "courses", (docs) => {
-            console.log("Found document:\n", docs);
+//         operation.findDocument(db, "courses", (docs) => {
+//             console.log("Found document:\n", docs);
 
-            operation.updateDocument(db, {name: "CS180"}, {description: "Intro to java programming"}, "courses", (result) => {
-                console.log("Updated Document:\n", result.result);
+//             operation.updateDocument(db, {name: "CS180"}, {description: "Intro to java programming"}, "courses", (result) => {
+//                 console.log("Updated Document:\n", result.result);
 
-                operation.findDocument(db, "courses", (docs) => {
-                    console.log("Found the updated document:\n", docs);
+//                 operation.findDocument(db, "courses", (docs) => {
+//                     console.log("Found the updated document:\n", docs);
 
-                    operation.deleteDocument(db, {name: "CS180"}, "courses", (result) => {
-                        console.log("delete document:\n", result.result);
+//                     operation.deleteDocument(db, {name: "CS180"}, "courses", (result) => {
+//                         console.log("delete document:\n", result.result);
                         
-                        db.dropCollection("courses", (err, result) => {
-                            assert.equal(err, null);
-                            console.log("Dropped colletion", result);
-                            client.close();
-                        })
-                    });
-                });
-            });
-        });
+//                         db.dropCollection("courses", (err, result) => {
+//                             assert.equal(err, null);
+//                             console.log("Dropped colletion", result);
+//                             client.close();
+//                         })
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
+
+
+mongoClient.connect(url).then((client) => {
+    console.log("connected to server");
+    const db = client.db(dbname);
+
+    operation.insertDocument(db,{name: "CS180", description: "Intro to java"}, "courses").then((result) => {
+        console.log("Inserted Document:\n", result.ops);
+        return operation.findDocument(db, "courses");
+    }).then((docs) => {
+        console.log("Found document:\n", docs);
+        return operation.updateDocument(db, {name: "CS180"}, {description: "Intro to java programming"}, "courses");
+    }).then((result) => {
+        console.log("Updated Document:\n", result.result);
+        return operation.findDocument(db, "courses");
+    }).then((docs) => {
+        console.log("Found the updated document:\n", docs);
+        return operation.deleteDocument(db, {name: "CS180"}, "courses");
+    }).then((result) => {
+        console.log("delete document:\n", result.result);
+        return db.dropCollection("courses");
+    }).then((result) => {
+        console.log("Dropped collection", result);
+        return client.close();
+    }).catch((err) => {
+        console.log(err);
     });
+}).catch((err) => {
+    console.log(err);
 });
--- mongo-node/operation.js
+++ mongo-node/operation.js
@@ -1,36 +1,47 @@
 const assert = require('assert');
 
-exports.insertDocument = (db, document, colletionName, callback) => {
+exports.insertDocument = (db, document, colletionName, _callback) => {
    var coll = db.collection(colletionName);
-   coll.insert(document, (err, result) => {
-       assert.equal(err, null);
-       console.log("Inserted: " + result.result.n + " documents into the collection " + colletionName);
-       callback(result);
-   }); 
+//    with Promise
+   return coll.insert(document); 
+
+//    coll.insert(document, (err, result) => {
+//        assert.equal(err, null);
+//        console.log("Inserted: " + result.result.n + " documents into the collection " + colletionName);
+//        callback(result);
+//    });
 };
 
-exports.findDocument = (db, colletionName, callback) => {
+exports.findDocument = (db, colletionName, _callback) => {
     var coll = db.collection(colletionName);
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);
-    });
+    // with Promise
+    return coll.find({}).toArray();
+
+    // coll.find({}).toArray((err, docs) => {
+    //     assert.equal(err, null);
+    //     callback(docs);
+    // });
 };
 
-exports.updateDocument = (db, document, update, colletionName, callback) => {
+exports.updateDocument = (db, document, update, colletionName, _callback) => {
     var coll = db.collection(colletionName);
-    coll.updateOne(document, {$set: update}, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Update the document with: ", update);
-        callback(result);
-    });
+    // with Promise
+    return coll.updateOne(document, {$set: update}, null);
+
+    // coll.updateOne(document, {$set: update}, null, (err, result) => {
+    //     assert.equal(err, null);
+    //     console.log("Update the document with: ", update);
+    //     callback(result);
+    // });
 };
 
-exports.deleteDocument = (db, document, colletionName, callback) => {
+exports.deleteDocument = (db, document, colletionName, _callback) => {
     var coll = db.collection(colletionName);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Deleted the document: ", document);
-        callback(result);
-    });
+    return coll.deleteOne(document);
+
+    // coll.deleteOne(document, (err, result) => {
+    //     assert.equal(err, null);
+    //     console.log("Deleted the document: ", document);
+    //     callback(result);
+    // });
 };
