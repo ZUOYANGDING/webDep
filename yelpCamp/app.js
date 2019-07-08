f203d36a6c6489e048efc33769e40a87d2d35627
zuoyangding <zuoyangding@yahoo.com>
2019-07-08 16:56:00 -0700

    comment page

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Campgrounds = require('./models/campground');
-var seedDB = require('./seeds');
+var express     =   require('express');
+var bodyParser  =   require('body-parser');
+var mongoose    =   require('mongoose');
+var Campgrounds =   require('./models/campground');
+var seedDB      =   require('./seeds');
+var Comment     =   require('./models/comment');
+
 
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
@@ -10,6 +12,7 @@ app.use(urlencodedParser);
 
 app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
 
 var connect = mongoose.connect("mongodb://localhost:27017/yelpcamp");
 connect.then(() => {
@@ -18,15 +21,17 @@ connect.then(() => {
     console.log(err);
 });
 
+// init DB
 seedDB();
 
+// routers for campgrounds
 app.get('/', function(req, res, next) {
     res.render("landing");
 }); 
 
 app.get('/campgrounds', function(req, res, next) {
     Campgrounds.find({}).then((campgrounds) => {
-        res.render("index", {campgrounds:campgrounds});
+        res.render("campgrounds/index", {campgrounds:campgrounds});
     }).catch((err) => {
         console.log(err);
     });
@@ -42,16 +47,53 @@ app.post('/campgrounds', function(req, res, next) {
 });
 
 app.get('/campgrounds/new', function(req, res, next) {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 app.get('/campgrounds/:id', function(req, res, next) {
-    Campgrounds.findById(req.params.id).then((campground) => {
-        res.render("show", {campground: campground});
+    Campgrounds.findById(req.params.id).populate("comments").then(function(campground) {
+        res.render("campgrounds/show", {campground: campground});
     }).catch((err) => {
         console.log(err);
     });
 });
+
+// routers for comments
+app.get('/campgrounds/:id/comments/new', function(req, res, next) {
+    Campgrounds.findById(req.params.id).then((campground) => {
+        if (campground) {
+            res.render("comments/new", {campground: campground});
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+});
+
+app.post('/campgrounds/:id/comments', function(req, res, next) {
+    Comment.create(req.body.comment).then((comment) => {
+        Campgrounds.findById(req.params.id).then((campground) => {
+            if (campground) {
+                campground.comments.push(comment);
+                campground.save().then((campground) => {
+                    console.log("add comment success");
+                }).catch((err) => {
+                    console.log(err);
+                });
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
+        res.statusCode = 200;
+        var redirectUrl = "/campgrounds/" + req.params.id;
+        res.redirect(redirectUrl);
+    }).catch((err) => {
+        console.log(err);
+    })
+});
+
+
+
+
 
 
 app.listen(app.get('port'), function() {
